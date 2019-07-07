e1555d65c39ae2ca458bfc90c8af2aed65e30c02
thirdgerb <thirdgerb@gmail.com>
2019-07-08 00:22:54 +0800

    调整配置

--- configs/chatbot.php
+++ configs/chatbot.php
@@ -1,7 +1,5 @@
 <?php
 
-use Commune\Chatbot\OOHost\Session\Scope;
-
 return [
     // debug 模式会记录更多的日志.
     'debug' => env('COMMUNE_DEBUG', true),
@@ -38,13 +36,7 @@ return [
         // 数据读写的组件, 用到了laravel DB 的redis 和 mysql
         \Commune\Chatbot\Laravel\Providers\LaravelDBServiceProvider::class,
     ],
-    'chatbotPipes' => [
-        'onUserMessage' => [
-            \Commune\Chatbot\App\ChatPipe\MessengerPipe::class,
-            \Commune\Chatbot\App\ChatPipe\ChattingPipe::class,
-            \Commune\Chatbot\OOHost\OOHostPipe::class,
-        ],
-    ],
+    'chatbotPipes' => \Commune\Chatbot\Config\Pipes\ChatbotPipesConfig::stub(),
     'translation' => [
         'loader' => 'php',
         'resourcesPath' => resource_path('/lang/chatbot'),
@@ -60,13 +52,7 @@ return [
         'permission' => NULL,
         'locking' => false,
     ],
-    'defaultMessages' => [
-        'platformNotAvailable' => 'system.platformNotAvailable',
-        'chatIsTooBusy' => 'system.chatIsTooBusy',
-        'systemError' => 'system.systemError',
-        'farewell' => 'dialog.farewell',
-        'messageMissMatched' => 'dialog.missMatched',
-    ],
+    'defaultMessages' => \Commune\Chatbot\Config\Message\DefaultMessagesConfig::stub(),
 
     // 在对话系统中注册的事件机制.
     'eventRegister' =>[
@@ -74,46 +60,7 @@ return [
     ],
 
     'host' => [
-        // 默认的对话语境.
         'rootContextName' => \Commune\Demo\App\Contexts\TestCase::class,
-        // 可回溯的对话断点数量.
-        'maxBreakpointHistory' => 10,
-        // 运行对话逻辑时, 语境变化的最大次数. 超过可能出现了重定向.
-        'maxRedirectTimes' => 20,
-        // session 过期时间.
-        'sessionExpireSeconds' => 3600,
-        // session 用到的数据缓存的时间.
-        'sessionCacheSeconds' => 60,
-        // session 中经历的管道.
-        'sessionPipes' => [
-
-            \Commune\Chatbot\App\SessionPipe\EventMsgPipe::class,
-            // 用户可用的命令.
-            Commune\Chatbot\App\Commands\UserCommandsPipe::class,
-            // 系统可用的命令.
-            Commune\Chatbot\App\Commands\AnalyserPipe::class,
-            // 本组件, 可以使用 #intentName# 直接命中某个意图, 主要用于测试.
-            \Commune\Chatbot\App\SessionPipe\MarkedIntentPipe::class,
-            // 优先级最高, 用于导航的意图中间件.
-            \Commune\Chatbot\App\SessionPipe\NavigationPipe::class,
-            // 使用rasa 匹配意图的中间件.
-            \Commune\Chatbot\App\Components\Rasa\RasaNLUPipe::class,
-        ],
-        // 这里的intent会对每一个请求进行强制的意图识别
-        // 命中的话优先执行.
-        'navigatorIntents' => [
-        ],
-        // session 预定义的记忆.
-        'memories' => [
-            [
-                'name' => 'sandbox',
-                'desc' => 'sandbox only used for test',
-                'scopes' => [Scope::SESSION_ID],
-                'entities' => [
-                    'test'
-                ]
-            ],
-        ],
-    ],
+    ] + \Commune\Chatbot\Config\Host\OOHostConfig::stub(),
 
 ];
